7d17bd4a3637fdd489c0bd42dd155781d2eeb0c5
Parwand <parwand.alsino@hhu.de>
2022-11-10 16:31:31 +0100

    added attributes and initial Methods to Seminarraum Entity

--- src/main/java/de/adesso/adessoseminarmanagement/domain/model/seminar/Seminarbuchung.java
+++ src/main/java/de/adesso/adessoseminarmanagement/domain/model/seminar/Seminarbuchung.java
@@ -1,4 +1,62 @@
 package de.adesso.adessoseminarmanagement.domain.model.seminar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
 public class Seminarbuchung {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long buchungsnummer;
+    private LocalDate buchungsdatum;
+
+    public Seminarbuchung() {
+    }
+
+    public Seminarbuchung(LocalDate buchungsdatum) {
+        this.buchungsdatum = buchungsdatum;
+    }
+
+    public Long getBuchungsnummer() {
+        return buchungsnummer;
+    }
+
+    public void setBuchungsnummer(Long buchungsnummer) {
+        this.buchungsnummer = buchungsnummer;
+    }
+
+    public LocalDate getBuchungsdatum() {
+        return buchungsdatum;
+    }
+
+    public void setBuchungsdatum(LocalDate buchungsdatum) {
+        this.buchungsdatum = buchungsdatum;
+    }
+
+    @Override
+    public String toString() {
+        return "Seminarbuchung{" +
+                "buchungsnummer=" + buchungsnummer +
+                ", buchungsdatum=" + buchungsdatum +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seminarbuchung that = (Seminarbuchung) o;
+
+        return Objects.equals(buchungsnummer, that.buchungsnummer);
+    }
+
+    @Override
+    public int hashCode() {
+        return buchungsnummer != null ? buchungsnummer.hashCode() : 0;
+    }
 }
--- src/main/java/de/adesso/adessoseminarmanagement/domain/model/seminar/Seminarraum.java
+++ src/main/java/de/adesso/adessoseminarmanagement/domain/model/seminar/Seminarraum.java
@@ -1,4 +1,71 @@
 package de.adesso.adessoseminarmanagement.domain.model.seminar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Seminarraum {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long raumnummer;
+    private Long maximalePersonenanzahl;
+    private String ausstattung;
+
+    public Seminarraum() {
+    }
+
+    public Seminarraum(Long maximalePersonenanzahl, String ausstattung) {
+        this.maximalePersonenanzahl = maximalePersonenanzahl;
+        this.ausstattung = ausstattung;
+    }
+
+    public Long getRaumnummer() {
+        return raumnummer;
+    }
+
+    public void setRaumnummer(Long raumnummer) {
+        this.raumnummer = raumnummer;
+    }
+
+    public Long getMaximalePersonenanzahl() {
+        return maximalePersonenanzahl;
+    }
+
+    public void setMaximalePersonenanzahl(Long maximalePersonenanzahl) {
+        this.maximalePersonenanzahl = maximalePersonenanzahl;
+    }
+
+    public String getAusstattung() {
+        return ausstattung;
+    }
+
+    public void setAusstattung(String ausstattung) {
+        this.ausstattung = ausstattung;
+    }
+
+    @Override
+    public String toString() {
+        return "Seminarraum{" +
+                "raumnummer=" + raumnummer +
+                ", maximalePersonenanzahl=" + maximalePersonenanzahl +
+                ", ausstattung='" + ausstattung + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Seminarraum that)) return false;
+
+        return Objects.equals(raumnummer, that.raumnummer);
+    }
+
+    @Override
+    public int hashCode() {
+        return raumnummer != null ? raumnummer.hashCode() : 0;
+    }
 }
