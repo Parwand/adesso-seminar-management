e976654c7da999d22ae1144377c3cfb4d073e746
Parwand <parwand.alsino@hhu.de>
2022-11-11 09:48:22 +0100

    added both annotations @Repository to 'PersonRepositoryImpl'

--- src/main/java/de/adesso/adessoseminarmanagement/infrastructure/db/person/PersonRepositoryImpl.java
+++ src/main/java/de/adesso/adessoseminarmanagement/infrastructure/db/person/PersonRepositoryImpl.java
@@ -2,7 +2,9 @@ package de.adesso.adessoseminarmanagement.infrastructure.db.person;
 
 import de.adesso.adessoseminarmanagement.applicationservice.repository.person.PersonRepository;
 import de.adesso.adessoseminarmanagement.domain.model.person.Person;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class PersonRepositoryImpl implements PersonRepository {
     private final PersonDao personDao;
 
