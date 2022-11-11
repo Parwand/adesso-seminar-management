1e8f54bdf9903cfe7252711bebb16ef01d4c8f33
Parwand <parwand.alsino@hhu.de>
2022-11-11 10:07:03 +0100

    created test for PersonService and tested save method

--- src/test/java/de/adesso/adessoseminarmanagement/applicationservice/service/person/PersonServiceTest.java
+++ src/test/java/de/adesso/adessoseminarmanagement/applicationservice/service/person/PersonServiceTest.java
@@ -1,4 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+package de.adesso.adessoseminarmanagement.applicationservice.service.person;
+
+import de.adesso.adessoseminarmanagement.domain.model.person.Adresse;
+import de.adesso.adessoseminarmanagement.domain.model.person.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class PersonServiceTest {
-  
+
+    @Autowired
+    private PersonService personService;
+
+    @Test
+    @DisplayName("person should be saved in DB")
+    void test_1() {
+        // Arrange
+        Person person = new Person();
+        person.setVorname("vorname1");
+        person.setNachname("nachname1");
+        person.setGeburtsdatum(LocalDate.of(1994, 3, 12));
+        Adresse adresse = new Adresse("Du", "Eller", "22a", "4055");
+        person.setAdresse(adresse);
+        // Act
+        personService.save(person);
+        // Assert
+        assertThat(person.getId()).isNotNull();
+    }
 }
