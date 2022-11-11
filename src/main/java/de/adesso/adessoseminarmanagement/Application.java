fad3d5ad86512139807b072ff729cdc1ea7fdb17
Parwand <parwand.alsino@hhu.de>
2022-11-11 15:59:37 +0100

    set author and date of project

--- src/main/java/de/adesso/adessoseminarmanagement/Application.java
+++ src/main/java/de/adesso/adessoseminarmanagement/Application.java
@@ -3,6 +3,10 @@ package de.adesso.adessoseminarmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Parwand Alsino
+ * 10.11.2022
+ */
 @SpringBootApplication
 public class Application {
 
