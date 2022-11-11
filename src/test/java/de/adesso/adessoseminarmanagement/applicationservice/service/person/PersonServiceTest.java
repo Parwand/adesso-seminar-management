e9dc9ed03b4dad4cdb3f33fbc65813283a6ed99e
Parwand <parwand.alsino@hhu.de>
2022-11-11 10:31:52 +0100

    person should be saved in DB with seminarList and seminarraum

--- src/test/java/de/adesso/adessoseminarmanagement/applicationservice/service/person/PersonServiceTest.java
+++ src/test/java/de/adesso/adessoseminarmanagement/applicationservice/service/person/PersonServiceTest.java
@@ -2,12 +2,17 @@ package de.adesso.adessoseminarmanagement.applicationservice.service.person;
 
 import de.adesso.adessoseminarmanagement.domain.model.person.Adresse;
 import de.adesso.adessoseminarmanagement.domain.model.person.Person;
+import de.adesso.adessoseminarmanagement.domain.model.seminar.Seminar;
+import de.adesso.adessoseminarmanagement.domain.model.seminar.Seminarraum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,6 +32,27 @@ class PersonServiceTest {
         person.setGeburtsdatum(LocalDate.of(1994, 3, 12));
         Adresse adresse = new Adresse("Du", "Eller", "22a", "4055");
         person.setAdresse(adresse);
+        // Act
+        personService.save(person);
+        // Assert
+        assertThat(person.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("person should be saved in DB with seminarList")
+    void test_2() {
+        // Arrange
+        Person person = new Person();
+        person.setVorname("vorname1");
+        person.setNachname("nachname1");
+        person.setGeburtsdatum(LocalDate.of(1994, 3, 12));
+        Adresse adresse = new Adresse("Du", "Eller", "22a", "4055");
+        Seminarraum seminarraum1 = new Seminarraum(10L, "All");
+        Seminar seminar1 = new Seminar("seminarTitle1",
+                LocalDateTime.of(LocalDate.of(2022, 11, 11), LocalTime.of(11, 0)),
+                LocalDateTime.of(LocalDate.of(2022, 11, 11), LocalTime.of(12, 0)), "kursinhalt", 10, "seminarleiter1",  "voraussetzung1", seminarraum1);
+        person.setAdresse(adresse);
+        person.setSeminarList(List.of(seminar1));
         // Act
         personService.save(person);
         // Assert
