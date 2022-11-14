fb329b7b8dae0be47bf5b36d9d95ec0b7bb7a495
Parwand <parwand.alsino@hhu.de>
2022-11-14 11:49:17 +0100

    tested with @ArchUnitTes: 'noClassesAccessToController'

--- src/test/java/de/adesso/adessoseminarmanagement/structure/ArchUnitTest.java
+++ src/test/java/de/adesso/adessoseminarmanagement/structure/ArchUnitTest.java
@@ -3,8 +3,26 @@ package de.adesso.adessoseminarmanagement.structure;
 
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
 import de.adesso.adessoseminarmanagement.Application;
+import de.adesso.adessoseminarmanagement.domain.model.person.Person;
+import de.adesso.adessoseminarmanagement.domain.model.seminar.Seminar;
+import de.adesso.adessoseminarmanagement.domain.model.seminar.Seminarbuchung;
+import de.adesso.adessoseminarmanagement.domain.model.seminar.Seminarraum;
+import org.springframework.stereotype.Controller;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 @AnalyzeClasses(packagesOf = Application.class, importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchUnitTest {
+
+    @ArchTest
+    static final ArchRule noClassesAccessToController = noClasses()
+            .that()
+            .resideOutsideOfPackage("..infrastructure.controller..")
+            .should()
+            .accessClassesThat()
+            .areAnnotatedWith(Controller.class);
 }
