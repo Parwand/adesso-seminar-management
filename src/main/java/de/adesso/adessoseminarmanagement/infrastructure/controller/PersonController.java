2f6dab5483e6df7d2222c2294f434a8b97a57ffa
Parwand <parwand.alsino@hhu.de>
2022-11-14 11:10:33 +0100

    added @GetMapping(/all) in 'PersonController'

--- src/main/java/de/adesso/adessoseminarmanagement/infrastructure/controller/PersonController.java
+++ src/main/java/de/adesso/adessoseminarmanagement/infrastructure/controller/PersonController.java
@@ -1,8 +1,14 @@
 package de.adesso.adessoseminarmanagement.infrastructure.controller;
 
 import de.adesso.adessoseminarmanagement.applicationservice.service.person.PersonService;
+import de.adesso.adessoseminarmanagement.domain.model.person.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/person")
@@ -12,5 +18,11 @@ public class PersonController {
 
     public PersonController(PersonService personService) {
         this.personService = personService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Person>> getAllPersons() {
+        List<Person> allPersons = personService.getAllPersons();
+        return new ResponseEntity<>(allPersons, HttpStatus.OK);
     }
 }
